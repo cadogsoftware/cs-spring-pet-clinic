2c569248853cd686567913103acf88fa646654f8
Richard Morgan <richard@cadogsoftware.co.uk>
2018-12-18 08:35:17 +0000

    Create initial project structure

---
+++ src/main/java/uk/co/cadogsoftware/csspringpetclinic/CsSpringPetClinicApplication.java
@@ -0,0 +1,14 @@
+package uk.co.cadogsoftware.csspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsSpringPetClinicApplication.class, args);
+	}
+
+}
+
