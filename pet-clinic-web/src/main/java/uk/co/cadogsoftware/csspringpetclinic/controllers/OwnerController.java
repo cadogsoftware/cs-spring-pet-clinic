27b081d89636e5ac339d7ff43f4537881fc0ce72
Richard Morgan <richard@cadogsoftware.co.uk>
2019-02-14 15:32:58 +0000

    Added first add owner endpoint

--- pet-clinic-web/src/main/java/uk/co/cadogsoftware/csspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/uk/co/cadogsoftware/csspringpetclinic/controllers/OwnerController.java
@@ -3,6 +3,7 @@ package uk.co.cadogsoftware.csspringpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.co.cadogsoftware.csspringpetclinic.model.Owner;
 import uk.co.cadogsoftware.csspringpetclinic.repositories.OwnerRepository;
 
 @Controller
@@ -20,5 +21,19 @@ public class OwnerController {
         model.addAttribute("owners", ownerRepository.findAll());
 
         return "owners"; // the view name
+    }
+
+    @RequestMapping("/owner/add")
+    public String addOwner(Model model) {
+
+        Owner owner = new Owner();
+        owner.setFirstName("Rich");
+        owner.setLastName("Mogs");
+
+        ownerRepository.save(owner);
+
+        model.addAttribute("owner", owner);
+
+        return "owner_added"; // the view name
     }
 }
